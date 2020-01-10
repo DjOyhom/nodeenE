bc2b65cd9a9c81c88c4e0470d9f8378826f481ee
rodrigo <rodrigo19-1_1998@hotmail.com>
2020-01-10 15:28:00 -0300

    sdadasdas

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('a ver los 5 ajajaj');
+});
+
+app.post('/', function (req, res) {
+  console.log(req.body);
 });
 
 app.get('/pagecount', function (req, res) {
   res.send('Hellonaa mentira World!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
 });
